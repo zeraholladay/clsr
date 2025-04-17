df49268fcec5514fb218cc7f141cf8bd5d138e32
Zera Holladay <zholladay@wayfair.com>
2025-04-17 18:42:23 -0400

    Finish cleanup

--- strutil.c
+++ strutil.c
@@ -3,30 +3,29 @@
 #include <stdlib.h>
 
 struct _node {
-    struct _node *left, *right;
-    char str[];
+  struct _node *left, *right;
+  char str[];
 } intern_root = {NULL, NULL, ""};
-  
+
 const char *sym_intern(const char *s, size_t s_len) {
-    struct _node *cur = &intern_root;
-  
-    while (cur) {
-      int cmp = strncmp_minlen(s, cur->str, s_len);
-      if (cmp == 0)
-        return cur->str;
-      cur = (cmp < 0) ? cur->left : cur->right;
-    }
-  
-    struct _node *n = malloc(sizeof(struct _node) + s_len + 1);
-    if (!n)
-      return NULL;
-  
-    n->left = n->right = NULL;
-  
-    char *cpy = strncpy(n->str, s, s_len);
-    if (!cpy)
-      return NULL;
-  
-    return cpy;
+  struct _node *cur = &intern_root;
+
+  while (cur) {
+    int cmp = strncmp_minlen(s, cur->str, s_len);
+    if (cmp == 0)
+      return cur->str;
+    cur = (cmp < 0) ? cur->left : cur->right;
   }
-  
+
+  struct _node *n = malloc(sizeof(struct _node) + s_len + 1);
+  if (!n)
+    return NULL;
+
+  n->left = n->right = NULL;
+
+  char *cpy = strncpy(n->str, s, s_len);
+  if (!cpy)
+    return NULL;
+
+  return cpy;
+}
--- sym_intern.h
+++
@@ -1,8 +0,0 @@
-#ifndef SYM_INTERN_H
-#define SYM_INTERN_H
-
-#include <stdlib.h>
-
-const char *sym_intern(const char *s, size_t s_len);
-
-#endif

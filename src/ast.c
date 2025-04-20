c76e2aca319455a1df32a014003aa6bffad9eb25
Zera Holladay <zeraholladay@gmail.com>
2025-04-19 21:44:19 -0400

    Lint

--- src/ast.c
+++ src/ast.c
@@ -1,60 +1,66 @@
 #include "ast.h"
 
 ASTNode *ast_new_literal_int(int i) {
-    ASTNode *n = NULL;
-    if (ALLOC(n)) die(LOCATION);
-    n->type = AST_Literal;
-    n->as.literal.type = Literal_Int;
-    n->as.literal.as.integer = i;
-    return n;
+  ASTNode *n = NULL;
+  if (ALLOC(n))
+    die(LOCATION);
+  n->type = AST_Literal;
+  n->as.literal.type = Literal_Int;
+  n->as.literal.as.integer = i;
+  return n;
 }
 
 ASTNode *ast_new_literal_sym(const char *sym) {
-    ASTNode *n = NULL;
-    if (ALLOC(n)) die(LOCATION);
-    n->type = AST_Literal;
-    n->as.literal.type = Literal_Sym;
-    n->as.literal.as.symbol = sym;
-    return n;
+  ASTNode *n = NULL;
+  if (ALLOC(n))
+    die(LOCATION);
+  n->type = AST_Literal;
+  n->as.literal.type = Literal_Sym;
+  n->as.literal.as.symbol = sym;
+  return n;
 }
 
 ASTNode *ast_new_empty_expr_list(void) {
-    ASTNode *n = NULL;
-    if (ALLOC(n)) die(LOCATION);
-    n->type = AST_List;
-    n->as.list.nodes = NULL;
-    n->as.list.count++;
-    return n;
+  ASTNode *n = NULL;
+  if (ALLOC(n))
+    die(LOCATION);
+  n->type = AST_List;
+  n->as.list.nodes = NULL;
+  n->as.list.count++;
+  return n;
 }
 
 ASTNode *ast_expr_list_append(ASTNode *list, ASTNode *item) {
-    size_t count = list->as.list.count;
+  size_t count = list->as.list.count;
 
-    ASTNode **new_items = NULL;
-    if (REALLOC_N(new_items, count + 1)) die(LOCATION);
+  ASTNode **new_items = NULL;
+  if (REALLOC_N(new_items, count + 1))
+    die(LOCATION);
 
-    new_items[count] = item;
+  new_items[count] = item;
 
-    list->as.list.nodes = new_items;
-    list->as.list.count++;
+  list->as.list.nodes = new_items;
+  list->as.list.count++;
 
-    return list;
+  return list;
 }
 
 ASTNode *ast_new_call(const PrimOp *prim, ASTNode *args) {
-    ASTNode *n = NULL;
-    if (ALLOC(n)) die(LOCATION);
-    n->type = AST_Call;
-    n->as.call.prim = prim;
-    n->as.call.args = args;
-    return n;
+  ASTNode *n = NULL;
+  if (ALLOC(n))
+    die(LOCATION);
+  n->type = AST_Call;
+  n->as.call.prim = prim;
+  n->as.call.args = args;
+  return n;
 }
 
 ASTNode *ast_new_closure(ASTNode *params, ASTNode *body) {
-    ASTNode *n = NULL;
-    if (ALLOC(n)) die(LOCATION);
-    n->type = AST_Closure;
-    n->as.closure.params = params;
-    n->as.closure.body = body;
-    return n;
+  ASTNode *n = NULL;
+  if (ALLOC(n))
+    die(LOCATION);
+  n->type = AST_Closure;
+  n->as.closure.params = params;
+  n->as.closure.body = body;
+  return n;
 }

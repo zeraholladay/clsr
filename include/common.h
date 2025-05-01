9e2ee21ec4d065fa90236dacb8e26d16293ead87
Zera Holladay <zeraholladay@gmail.com>
2025-05-01 08:12:52 -0400

    Fussing ...

--- include/common.h
+++ include/common.h
@@ -4,46 +4,42 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-/*
-debugging
-*/
-
-#ifndef DEBUG
-#define DEBUG_ENABLED 0
+#ifndef CLSR_DEBUG
+#define CLSR_DEBUG_ENABLED 0
 #endif
 
-#ifndef DEBUG
-#define DEBUG(msg)                                                             \
+#ifndef debug
+#define debug(msg)                                                             \
   do {                                                                         \
-    if (DEBUG_ENABLED)                                                         \
+    if (CLSR_DEBUG_ENABLED)                                                    \
       fprintf(stderr, "[%s:%d] %s(): %s\n", __FILE__, __LINE__, __func__,      \
               msg);                                                            \
   } while (0)
 #endif
 
-/* LOCATION */
-
 #define STRINGIFY_HELPER(x) #x
 #define STRINGIFY(x) STRINGIFY_HELPER(x)
-#define LOCATION __FILE__ ":" STRINGIFY(__LINE__)
+#define LOCATION "[" __FILE__ ":" STRINGIFY(__LINE__) "] "
 
 /*
-memory management
+memory and string management
 Gnulib - The GNU Portability Library
 https://git.savannah.gnu.org/git/gnulib.git
 */
+
+#define container_of(ptr, type, member)                                        \
+  ((type *)((char *)(ptr) - offsetof(type, member)))
 
 inline static int safe_alloc_check(void *ptr) {
   /* Return 0 if the allocation was successful, -1 otherwise.  */
   return -!ptr;
 }
 
-#define ALLOC(ptr) ALLOC_N(ptr, 1)
-
 #define ALLOC_N(ptr, count)                                                    \
   safe_alloc_check((ptr) = calloc(count, sizeof *(ptr)))
+
+#define ALLOC(ptr) ALLOC_N(ptr, 1)
 
 #define REALLOC_N(ptr, count)                                                  \
   safe_alloc_check((ptr) = realloc(ptr, (count) * sizeof *(ptr)))
@@ -55,9 +51,6 @@ static inline void die(const char *msg) {
   exit(1);
   abort();
 }
-
-#define container_of(ptr, type, member)                                        \
-  ((type *)((char *)(ptr) - offsetof(type, member)))
 
 /* Strings */
 

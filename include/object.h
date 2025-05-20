9d4517506d09e4df325c695470fffad087e56d77
Zera Holladay <zholladay@wayfair.com>
2025-05-20 12:00:28 -0400

    interim commit

--- include/object.h
+++ include/object.h
@@ -5,24 +5,48 @@
 #include "list.h"
 
 typedef struct Type Type;
+typedef struct Object Object;
 
-typedef struct Object
+typedef Object *(*Function) (Object *self, List *args, Dict *kwargs);
+
+typedef enum
+{
+  TYPE_NONE,      // special constant
+  TYPE_SYMBOL,    // identifiers
+  TYPE_INTEGER,   // literal
+  TYPE_STRING,    // literal
+  TYPE_LIST,      // list
+  TYPE_DICT,      // dict
+  TYPE_BUILTIN,   // builtin fn
+  TYPE_LAMBDA     // user-defined fn
+} Atomics;
+
+typedef struct object
+{
+  Atomics atomic_type;
+  union
+  {
+    Function fn;
+  };
+  
+} Atom;
+
+struct Object
 {
   Type *type;
   Dict *attrs;
-} Object;
-
-typedef Object *(*InitFn) (Type *type, List *args, Dict *kwargs);
+};
 
 struct Type
 {
   const char *type_name;
   List *bases;
   Dict *attrs;
-  InitFn init_fn;
 };
 
-Dict *type_init (void);
+Dict *init_types (void);
+Object *call (const char *fn_name, Object *self, List *args, Dict *kwargs);
+Object *new (Type *type, List *args, Dict *kwargs);
 Type *type (Dict *type_dict, const char *type_name, List *bases, Dict *attrs);
 
 #endif
--- src/object.c
+++ src/object.c
@@ -3,7 +3,72 @@
 #include "object.h"
 
 static Object *
-init_fn (Type *type, List *args, Dict *kwargs)
+eq_wrapper (Object *self, List *args, Dict *kwags)
+{
+
+}
+
+static Object *
+init_wrapper (Object *self, List *args, Dict *kwags)
+{
+  
+}
+
+static Object *
+to_str_wrapper (Object *self, List *args, Dict *kwags)
+{
+  
+}
+
+static Atom eq_wrapper_atom = {
+  .atomic_type = TYPE_BUILTIN,
+  .fn = eq_wrapper,
+};
+
+static Type *object_type = NULL;
+
+Dict *
+init_types (void)
+{
+  Dict *type_dict = dict_alloc (NULL, 0);
+
+  if (!type_dict)
+    {
+      return NULL;
+    }
+
+  Dict *object_attrs = dict_alloc (
+      (DictEntity[]){
+          DICT_ENTITY ("eq", &eq_wrapper_atom),
+          DICT_ENTITY ("init_fn", NULL),
+          DICT_ENTITY ("to_str", NULL),
+      },
+      3);
+
+  object_type = type(type_dict, "object", NULL, object_attrs);
+
+  return type_dict;
+}
+
+Object *
+call (const char *fn_name, Object *self, List *args, Dict *kwargs)
+{
+  Type *type = self->type;
+
+  DictEntity *attrs = dict_lookup (type->attrs, fn_name);
+
+  if (!attrs) // TODO: superclass
+    {
+      return NULL;
+    }
+
+  Function fn = attrs->val;
+
+  return fn (self, args, kwargs);
+}
+
+Object *
+new (Type *type, List *args, Dict *kwargs)
 {
   Object *new_obj = malloc (sizeof *(new_obj));
 
@@ -14,25 +79,21 @@ init_fn (Type *type, List *args, Dict *kwargs)
 
   new_obj->type = type;
 
-  InitFn init_fn = dict_lookup(type, "init_fn");
-
-  if (!init_fn)
-    {
-      return NULL;
-    }
-
-  return init_fn(type, args, kwargs);
-}
-
-Dict *
-type_init (void)
-{
-  return dict_alloc (NULL, 0);
+  return call ("init_fn", new_obj, args, kwargs);
 }
 
 Type *
 type (Dict *type_dict, const char *type_name, List *bases, Dict *attrs)
 {
+  // types are immutable for now
+  DictEntity *entity = dict_lookup(type_dict, type_name);
+
+  if (entity)
+    {
+      return entity->val;
+    }
+
+  // otherwise create new type
   Type *new_type = malloc (sizeof *(new_type));
 
   if (!new_type)
@@ -43,7 +104,6 @@ type (Dict *type_dict, const char *type_name, List *bases, Dict *attrs)
   new_type->type_name = type_name;
   new_type->bases = bases;
   new_type->attrs = attrs;
-  new_type->init_fn = NULL; // TODO
 
   int res = dict_insert (type_dict, type_name, new_type);
 
